1d7f11aacf140b1ff703d9fb5a0e17ba3c137e06
Ravindu <ravinduamaya@gmail.com>
2020-03-19 13:18:20 +0530

    ravindu

---
+++ app/src/main/java/com/example/sliitfeedback/AddlogR.java
@@ -0,0 +1,14 @@
+package com.example.sliitfeedback;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddlogR extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_log);
+    }
+}
---
+++ app/src/main/java/com/example/sliitfeedback/TeachersFeedback.java
@@ -0,0 +1,14 @@
+package com.example.sliitfeedback;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TeachersFeedback extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.teacher_feedback);
+    }
+}
