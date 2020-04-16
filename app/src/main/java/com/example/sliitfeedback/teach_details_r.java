9346bee99a01e90ac9914a8352cd6320a6f640f7
LahiruNikolin <nikolinlahiru@gmail.com>
2020-04-16 19:25:51 +0530

    bump3

--- app/src/main/java/com/example/sliitfeedback/teach_details_r.java
+++ app/src/main/java/com/example/sliitfeedback/teach_details_r.java
@@ -3,6 +3,9 @@ package com.example.sliitfeedback;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class teach_details_r extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class teach_details_r extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teach_details_r);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.teacher_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
