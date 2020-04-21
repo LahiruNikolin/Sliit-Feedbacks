7889607b4cd8ceea32ab0182a82ef73134a2d129
Ravindu <ravinduamaya@gmail.com>
2020-04-21 16:19:48 +0530

    Merge branch 'master' of C:\Users\Ravindu\AndroidStudioProjects\Sliit-Feedbacks-AdminApp with conflicts.

--- app/src/main/java/com/example/sliitfeedback/AdminProfileR.java
+++ app/src/main/java/com/example/sliitfeedback/AdminProfileR.java
@@ -4,13 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class admin_log_r extends AppCompatActivity {
+public class AdminProfileR extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_log_r);
+        setContentView(R.layout.activity_admin_profile_r);
     }
 }
--- app/src/main/java/com/example/sliitfeedback/CreateAdmin.java
+++ app/src/main/java/com/example/sliitfeedback/CreateAdmin.java
@@ -1,14 +1,95 @@
 package com.example.sliitfeedback;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CreateAdmin extends AppCompatActivity {
+    EditText Email,Password,ConPassword;
+    Button CreateAdmin;
+    ProgressBar progressBar;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_admin);
+
+        Email = findViewById(R.id.email);
+        Password = findViewById(R.id.Password);
+        ConPassword = findViewById(R.id.ConPassword);
+        CreateAdmin = findViewById(R.id.Register);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        progressBar = findViewById(R.id.progressBar);
+
+        if(fAuth.getCurrentUser()!=null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class ));
+            finish();
+        }
+
+        CreateAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = Email.getText().toString().trim();
+                String password = Password.getText().toString().trim();
+                String conPW = ConPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Email.setError("Email is required");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    Password.setError("Password is required");
+                    return;
+                }
+                if (password.length() < 6) {
+                    Password.setError("Password must be above 6 characters");
+                    return;
+                }
+                if (password != conPW) {
+                    Password.setError("Password doesn't match");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(CreateAdmin.this, "User Created...", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class ));
+
+
+                        }else{
+                            Toast.makeText(CreateAdmin.this, "Eror !" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+
+            });
+
+            }
+        });
+
+
+
+
     }
 }
